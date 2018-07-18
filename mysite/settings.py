c010d8f6d88a0dba975e37d5cb4ad3954d15cae2
Manjari Sengar <msengar@spncore.local>
2018-07-18 13:55:19 -0500

    actually sending this time

--- mysite/settings.py
+++ mysite/settings.py
@@ -131,6 +131,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+os.path.join(BASE_DIR, "static"),
+'/mysite/static/',
+]
+
 
 LOGIN_REDIRECT_URL = '/'
 
